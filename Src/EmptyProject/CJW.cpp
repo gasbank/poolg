c5a1728ba202022f3dc4252ae742f8ee49a6fbf4
Jaewoo Kim <dizzyherb@hotmail.com>
2008-07-26 12:19:56 +0000

    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@42 20279357-db7c-4b2f-af4c-8ae08146ad34

--- Src/EmptyProject/CJW.cpp
+++ Src/EmptyProject/CJW.cpp
@@ -4,7 +4,7 @@
 void CJW::init (LPDIRECT3DDEVICE9& d3dDev)
 {
 	m_backGround.init(L"bg01.jpg", d3dDev);
-	m_backGround.setPosition (-0.5f, -0.5f, 20);
+	m_backGround.setPosition (-50.0f, -25.0f, 20);
 	m_backGround.setSize(100, 50);
 
 	m_hpbar.init(L"hpBar.bmp", d3dDev);
@@ -13,19 +13,19 @@ void CJW::init (LPDIRECT3DDEVICE9& d3dDev)
 	m_hpbar.setPosition (0, 10, 5);
 
 	m_StatusBoxPlayer.init(L"statusWindow.bmp", d3dDev);
-	m_StatusBoxPlayer.setPosition (1.0f, 0.8f, 7);
+	m_StatusBoxPlayer.setPosition (11.0f, 7.0f, 7);
 	m_StatusBoxPlayer.setSize(10, 8);
 
 	m_StatusBoxEnemy.init(L"statusWindow.bmp", d3dDev);
-	m_StatusBoxEnemy.setPosition (-2.0f, -1.8f, 7);
+	m_StatusBoxEnemy.setPosition (-21.0f, -15.0f, 7);
 	m_StatusBoxEnemy.setSize(10, 8);
 
 	m_Player.init(L"poolG_r.jpg", d3dDev);
-	m_Player.setPosition (0.5f, -0.8f, 10);
+	m_Player.setPosition (5.0f, -14.0f, 10);
 	m_Player.setSize(15.5f, 17.4f);
 
 	m_Enemy.init(L"poolG_l.jpg", d3dDev);
-	m_Enemy.setPosition (-2.5f, -0.2f, 10);
+	m_Enemy.setPosition (-20.0f, 7.0f, 10);
 	m_Enemy.setSize(7.75f, 8.7f);
 }
 

bafca2899e2f84d9b3b1f3d9c9feffb89ac4ff63
Jaewoo Kim <dizzyherb@hotmail.com>
2008-07-31 13:36:50 +0000

    UI 폰트 추가.
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@80 20279357-db7c-4b2f-af4c-8ae08146ad34

--- Src/EmptyProject/Battle.cpp
+++ Src/EmptyProject/Battle.cpp
@@ -3,11 +3,8 @@
 
 void Battle::init (LPDIRECT3DDEVICE9& d3dDev, int scrWidth, int scrHeight)
 {
+	HRESULT hr;
 	m_pDev = d3dDev;
-
-	m_backGround.init(L"BattleUI\\BGcampus.bmp", d3dDev);
-	m_backGround.setPosition (-(float)scrWidth/2, -(float)scrHeight/2, 20);
-	m_backGround.setSize((float)scrWidth/2, (float)scrHeight/2);
 
 	float statusBoxWidth = 163;
 	float statusBoxHeight = 124;
@@ -23,19 +20,27 @@ void Battle::init (LPDIRECT3DDEVICE9& d3dDev, int scrWidth, int scrHeight)
 	m_StatusBoxEnemy.init(L"BattleUI\\StatusBox.png", d3dDev);
 	m_StatusBoxEnemy.setPosition (statusBoxEnemysPositionX, statusBoxEnemysPositionY, 6.9f);
 	m_StatusBoxEnemy.setSize(statusBoxWidth, statusBoxHeight);
+	
+	int skillBoxHeight = 200;
+	int skillBoxWidth = skillBoxHeight * 593 / 933;
+	int skillBoxPositionX = scrWidth/2 -skillBoxWidth - 3;
+	int skillBoxPositionY =  -scrHeight/2 + 3;
+	m_SkillBox.init(L"BattleUI\\SkillBox.png", d3dDev);
+	m_SkillBox.setPosition ((float)skillBoxPositionX, (float)skillBoxPositionY, 6.9f);
+	m_SkillBox.setSize((float)skillBoxWidth, (float)skillBoxHeight);
 
-	m_SkillBox.init(L"BattleUI\\SkillBox.jpg", d3dDev);
-	m_SkillBox.setPosition (statusBoxEnemysPositionX, statusBoxEnemysPositionY, 6.9f);
-	m_SkillBox.setSize(statusBoxWidth, statusBoxHeight);
+	int skillContentBoxHeight = 200;
+	int skillContentBoxWidth = skillContentBoxHeight * 390 / 269;
+	m_SkillContentBox.init(L"BattleUI\\SkillContentBox.png", d3dDev);
+	m_SkillContentBox.setPosition ((float)skillBoxPositionX - skillContentBoxWidth, (float)skillBoxPositionY, 6.9f);
+	m_SkillContentBox.setSize((float)skillContentBoxWidth, (float)skillContentBoxHeight);
 
-	m_SkillContentBox.init(L"BattleUI\\SkillContent.jpg", d3dDev);
-	m_SkillContentBox.setPosition (statusBoxEnemysPositionX, statusBoxEnemysPositionY, 6.9f);
-	m_SkillContentBox.setSize(statusBoxWidth, statusBoxHeight);
-
-	m_DialogBox.init(L"BattleUI\\DialogBox.jpg", d3dDev);
-	m_DialogBox.setPosition (statusBoxEnemysPositionX, statusBoxEnemysPositionY, 6.9f);
-	m_DialogBox.setSize(statusBoxWidth, statusBoxHeight);
-
+	float dialogBoxWidth = (float)(int)(scrWidth - statusBoxWidth - 30);
+	float dialogBoxHeight = 124;
+	m_DialogBox.init(L"BattleUI\\DialogBox.png", d3dDev);
+	m_DialogBox.setPosition ((float)-(scrWidth)/2 + 10, (float)(scrHeight)/2 - dialogBoxHeight - 10, 6.9f);
+	//m_DialogBox.setPosition (10, 10, 6.9f);
+	m_DialogBox.setSize(dialogBoxWidth, dialogBoxHeight);
 
 	float statusBarWidth = statusBoxWidth * 0.67f;
 	float statusBarHeight = statusBarWidth * 0.1f;
@@ -85,6 +90,13 @@ void Battle::init (LPDIRECT3DDEVICE9& d3dDev, int scrWidth, int scrHeight)
 	m_mpBarEnemy.setPosition (statusBoxEnemysPositionX + statusBoxWidth*0.23f, statusBoxEnemysPositionY + statusBoxHeight * 0.65f, 4.5f);
 	m_mpBarEnemy.setSize(statusBarWidth, statusBarHeight);
 
+	V( D3DXCreateFont(d3dDev, 17, 0, FW_NORMAL, 1, FALSE, DEFAULT_CHARSET, OUT_RASTER_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, _T("HYnamL"), &m_lblHYnamL) );
+	V( D3DXCreateFont(d3dDev, 18, 0, FW_NORMAL, 1, FALSE, DEFAULT_CHARSET, OUT_RASTER_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, _T("Rockwell Extra Bold"), &m_lblREB) );
+
+
+
+
+
 	//[윤욱]
 	m_menu.init(L"menu.png", d3dDev);
 	m_menu.setPosition (8.5f, -2.0f, 4);
@@ -93,17 +105,10 @@ void Battle::init (LPDIRECT3DDEVICE9& d3dDev, int scrWidth, int scrHeight)
 	m_selc.init(L"triangle.png", d3dDev);
 	m_selc.setPosition (9.5f, 11.2f, 4);
 	m_selc.setSize(0.75, 1.5);
-
-
-
-
-	
-
 }
 
 void Battle::frameMove(float fElapsedTime)
 {
-	m_backGround.frameMove(fElapsedTime);
 	m_StatusBoxPlayer.frameMove(fElapsedTime);
 	m_StatusBoxEnemy.frameMove(fElapsedTime);
 	m_SkillBox.frameMove(fElapsedTime);
@@ -132,7 +137,7 @@ void Battle::frameMove(float fElapsedTime)
 	//m_Enemy.frameMove(fElapsedTime);
 }
 
-void Battle::draw()
+void Battle::draw(int scrWidth, int scrHeight)
 {
 	D3DMATERIAL9 material;
 	D3DCOLORVALUE cv, cv2;
@@ -145,7 +150,6 @@ void Battle::draw()
 	material.Specular = cv2;
 	m_pDev->SetMaterial(&material);
 
-	m_backGround.draw();
 	m_StatusBoxPlayer.draw();
 	m_StatusBoxEnemy.draw();
 	m_SkillBox.draw();
@@ -161,6 +165,8 @@ void Battle::draw()
 	m_expBarPlayer.draw();
 	m_hpBarEnemy.draw();
 	m_mpBarEnemy.draw();
+
+	drawFixedText(scrWidth, scrHeight);
 	
 //	D3DXMATRIX mRot, mScale, mTrans, mWorld;
 //	D3DXMatrixRotationZ(&mRot, D3DXToRadian(45));
@@ -189,7 +195,6 @@ void Battle::handleMessages(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 void Battle::release ()
 {
-	m_backGround.release();
 	m_StatusBoxPlayer.release();
 	m_StatusBoxEnemy.release();
 	m_SkillBox.release();
@@ -213,4 +218,56 @@ void Battle::release ()
 	//[윤욱]
 	m_menu.release();
 	m_selc.release();
+
+	SAFE_RELEASE( m_lblHYnamL );
+	SAFE_RELEASE( m_lblREB);
+}
+
+void Battle::drawFixedText(int scrWidth, int scrHeight)
+{
+	WCHAR textBuffer[512];
+	RECT rc;
+	rc.top = 0;
+	rc.left = 0;
+	rc.right = scrWidth;
+	rc.bottom = scrHeight;
+	StringCchPrintf(textBuffer, 512, L"TextTest!!!!");
+	m_lblHYnamL->DrawTextW(0, textBuffer, -1, &rc, DT_NOCLIP | DT_LEFT, D3DXCOLOR( 0.0f, 1.0f, 0.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(textBuffer, 512, L"HYnamL 글씨체의 묘미를 맘껏 느껴봅시다.");
+	m_lblHYnamL->DrawTextW(0, textBuffer, -1, &rc, DT_NOCLIP | DT_LEFT, D3DXCOLOR( 0.0f, 1.0f, 0.0f, 1.0f ) );
+
+
+	float statusBoxPlayersPositionX = 10;
+	float statusBoxPlayersPositionY = scrHeight - 10;
+	rc.top = statusBoxPlayersPositionY - 102;
+	rc.left = statusBoxPlayersPositionX + 5;
+	StringCchPrintf(textBuffer, 512, L"HP");
+	m_lblREB->DrawTextW(0, textBuffer, -1, &rc, DT_NOCLIP | DT_LEFT, D3DXCOLOR( 1.0f, 1.0f, 1.0f, 1.0f ) );
+
+/*
+	
+	StringCchPrintf(debugBuffer, 512, L"カメラの位置: (%.2f, %.2f, %.2f)", g_camera.GetEyePt()->x, g_camera.GetEyePt()->y, g_camera.GetEyePt()->z);
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 0.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L"カメラの眺める場所: (%.2f, %.2f, %.2f)", g_camera.GetLookAtPt()->x, g_camera.GetLookAtPt()->y, g_camera.GetLookAtPt()->z);
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 0.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L"タイルの位置: (%.2f, %.2f, %.2f)", g_pic.getPos()->x, g_pic.getPos()->y, g_pic.getPos()->z);
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 0.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L" - 상하좌우 화살표키를 이용해 카메라를 좌우/가까이멀리 이동 가능");
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 1.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L"   (카메라는 항상 +Z 축을 바라보게 됨)");
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 1.0f, 1.0f, 1.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L" - I/K/J/L키를 이용해 맵 이미지를 상하좌우로 이동 가능");
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 1.0f, 1.0f ) );
+	rc.top += 12;
+	StringCchPrintf(debugBuffer, 512, L" - 스페이스키를 이용해 특수기능 작동 (누르고 있으면 필살공격)");
+	g_pFont->DrawTextW(0, debugBuffer, -1, &rc, DT_NOCLIP | DT_RIGHT, D3DXCOLOR( 0.0f, 1.0f, 1.0f, 1.0f ) );
+}
+
+*/
 }

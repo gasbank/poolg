b5cc5d41279b68616e0bf0df9b4fb191bc6e4404
Minseok Baek <painl2@dreamwiz.com>
2008-08-13 13:50:37 +0000

    Dialog가 대량으로 추가되었습니다! 풀쥐 기획안이 올라왔습니다!
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@302 20279357-db7c-4b2f-af4c-8ae08146ad34

--- Working/library/EpDialog1.tcl
+++ Working/library/EpDialog1.tcl
@@ -1,25 +1,21 @@
-set EpDialogList [list EpDialog1 EpDialog2 EpDialog3 EpDialogStop EpDialogHeal];
+set EpDialogList [list EpDialog1 EpDialog2 EpDialog3 EpDialog4 EpDialogStop EpDialogHeal EpDialogBox EpDialogMirror EpDialogLaserOff EpDialogLaserOn EpDialogSave];
 
 namespace eval EpDialog1 {
 	
 	set region [ list 9 9 -1 -1 ]; ;# left, top, right, bottom
-	set oneTime 0;
+	set oneTime 1;
 	
 	set player "PoolG"
-	set enemy "YangSanHyung"
+	set npc1 "GlooP"
+	set npc2 "GetG"
 	
 	set dialog [ list\
-		$player		"내 이름은 풀쥐야!! 난 (9,9) 타일에 서서 대화를 시작하겠어."\
-		$enemy		"그게 뭐 어쨌다는거임?"\
-		$player		"그냥 그렇다고.."\
-		$enemy		"이런 식으로 대화를 계속 하는게 의미가 있는지 생각해보자"\
-		$player		"그래. 내 생각엔 적어도 20번 이상 대화를 주고 받아야 사용자가 납득할만한 대화 내용이 된다고 생각해."\
-		$enemy		"요즘같이 바쁜 세상에 이미 눈치빠른 플레이어들은 이정도면 대화가 잘 구현됐다고 생각할거야."\
-		$player		"하지만 드라군이 출동하면 어떨까?"\
-		$enemy		"......"\
-		$player		"내 개그를 이해 못했군. 이건 요즘 DG인사이드에 유행하는 개그야"\
-		$enemy		"Shut up"\
-		$player		"오 양키어도 구사하는군"\
+		$npc1		"PoolC에서 대청소를 해버리는 바람에, 우리들의 삶의 터전이 좁아저 버렸다네"
+		$player		"뭬야!"
+		$npc1		"게다가 앞으론 우리들을 살 수 없도록 언제나 깨끗하게 유지하겠다고 하더군"
+		$player		"맙소사! 난 가만히 있을 수 없어"
+		$npc2		"제발 그들을 말려주게"
+		$player		"이대로 가만히 있을 순 없지. 다녀오겠네"
 	];	
 }
 
@@ -29,29 +25,54 @@ namespace eval EpDialog2 {
 	set region [ list 10 10 -1 -1 ]; ;# left, top, right, bottom
 	set oneTime 1;
 	
-	set player "풀쥐한글판"
-	set enemy "양산형"
+	set player "PoolG"
+	set npc "김두부"
 	
 	set dialog [ list\
-		$player		"내 이름은 풀쥐야!! 난 (10,10) 타일에 서서 대화를 시작하겠어. 이번엔 귀찮으니깐 짧게 끝내자."\
-		$enemy		"Okay."\
-		$player		"지금은 한글판이거든?"\
-		$enemy		"S*it! Speak English you idiot!"\
+		$player		"너는 누구냐!"\
+		$npc		"나는 두부를 지극히 사랑하는 이 동아리의 부원이라네"\
+		$player		"이 동아리에 소속된 사람이라면 대청소를 한 주범이 누군지 알고 있을텐데?"\
+				"순순히 말하지 않으면 널 Overflow 시켜버리겠어!"\
+		$npc		"대청소를 한 미화부장이라면, A213이라는 신비의 장소에서 코딩을 즐겨 한다고 하더군"\
+		$player		"말이 통해서 다행이군"\
 	];	
 }
 
 namespace eval EpDialog3 {
 	
 	set region [ list 18 9 -1 -1 ]; ;# left, top, right, bottom
-	set oneTime 0;
+	set oneTime 1;
 	
 	set player "PoolG"
-	set enemy "YangSanHyung"
+		
+	set dialog [ list\
+		$player		"도대체 이 끔찍한 시설의 컴퓨터 실은 뭐냔 말이다....."\
+				"정말 이런 곳에서 코딩을 할 수 있단 말인가! 제길, 아까 그녀석이 잘못 가르쳐 준거 아냐?"\
+				"만약 미화부장이 없다면 그녀석을 혼내러 가야겠어"\
+	];	
+}
+
+namespace eval EpDialog4 {
+	
+	set region [ list 9 9 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 1;
+	
+	set player "PoolG"
+	set boss "괴물"
 	
 	set dialog [ list\
-		$enemy		"넌 누구니?"\
-		$player		"어차피 죽을 사람에게 이름을 가르쳐줄 필요는 없겠지..."\
-		$enemy		"덤벼라!"\
+		$player		"당신이 PoolC의 미화부장 인가!"\
+		$boss		"난 총무부장이야"\
+		$player		"뭐! 그렇다면 미화부장은 누구란 말이야!"\
+		$boss		"난 미화부장이야"\
+		$player		"정체가 뭐야"\
+		$boss		"난 PoolC의 미화부장겸 총무부장이지"\
+		$player		"그렇다면 당신이 우리들의 삶의 터전을 없애버렸단 말이군"\
+		$boss		"동방은 청결하게 유지되어야 하거든"\
+		$player		"용서할 수 없다! 우리들의 삶의 터전을 돌려달라!"\
+		$boss		"Shut up"\
+		$player		"오 양키어도 구사하는군"\
+		$boss		"다 찢어버리겠어!"\
 	];	
 }
 
@@ -63,11 +84,35 @@ namespace eval EpDialogStop {
 	set player "SYSTEM"
 	
 	set dialog [ list\
-		$player		"여기로 오면 안 생긴다네."\
+		$player		"여기로 오면 안 생긴다네"\
 	];	
 }
 
 namespace eval EpDialogHeal {
+	
+	set region [ list 15 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"온 몸이 편안해지는 기분이다"\
+	];	
+}
+
+namespace eval EpDialogBox {
+	
+	set region [ list 16 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"뭔가 밀 수 있어보이는 상자가 놓여져있다"\
+	];	
+}
+
+namespace eval EpDialogMirror {
 	
 	set region [ list 17 14 -1 -1 ]; ;# left, top, right, bottom
 	set oneTime 0;
@@ -75,6 +120,54 @@ namespace eval EpDialogHeal {
 	set player "SYSTEM"
 	
 	set dialog [ list\
-		$player		"힐링 트랩이다! 넌 이제 죽은 목숨이지."\
+		$player		"돌릴 수 있도록 만들어진 거울이다"\
+	];	
+}
+
+namespace eval EpDialogLaserOff {
+	
+	set region [ list 18 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"레이져 포인터가 꺼져있다"\
+	];	
+}
+
+namespace eval EpDialogLaserOn {
+	
+	set region [ list 19 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"레이져 포인터에서 레이져가 나오고 있다"\
+	];	
+}
+
+namespace eval EpDialogTarget {
+	
+	set region [ list 20 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"레이져에만 반응하는 센서인 듯 하다"\
+	];	
+}
+
+namespace eval EpDialogSave {
+	
+	set region [ list 21 14 -1 -1 ]; ;# left, top, right, bottom
+	set oneTime 0;
+	
+	set player "SYSTEM"
+	
+	set dialog [ list\
+		$player		"지금까지의 기록을 저장할 수 있습니다"\
 	];	
 }

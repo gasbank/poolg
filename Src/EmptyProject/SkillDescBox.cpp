176be9833ce431b41ad1cd330401aa63d0abf85a
Jaewoo Kim <dizzyherb@hotmail.com>
2008-08-15 05:42:13 +0000

    - Tsumimasaeungㅠㅠㅠ
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@339 20279357-db7c-4b2f-af4c-8ae08146ad34

---
+++ Src/EmptyProject/SkillDescBox.cpp
@@ -0,0 +1,62 @@
+#include "EmptyProjectPCH.h"
+#include "SkillDescBox.h"
+
+void SkillDescBox::setOnPos(float x, float y, float z)
+{
+	m_vPos.x = x + m_distance; m_vPos.y = y; m_vPos.z = z; m_onPosX = x;
+}
+
+void SkillDescBox::onBox()
+{
+	/*다른 상태일 때만 작동한다.*/
+	if (m_isOn == false)
+		m_movingDirect = true;
+}
+
+void SkillDescBox::offBox()
+{
+	if (m_isOn == true)
+		m_movingDirect = false;
+}
+
+bool SkillDescBox::isOn()
+{
+	return m_isOn&&m_movingDirect;
+}
+
+void SkillDescBox::frameMove( float fElapsedTime )
+{	
+	float movingRate = 0.2f;
+	float curDistance = m_vPos.x - m_onPosX; 
+
+	/*왼쪽으로 움직인다.*/
+	if (m_movingDirect == true && m_isOn == false)
+	{
+		if (curDistance <= 0.01f)
+		{
+			m_vPos.x = m_onPosX;
+			m_isOn = true;
+		}
+		else
+			m_vPos.x -= (m_vPos.x - m_onPosX) * movingRate + movingRate;
+	}
+	/*오른쪽으로 움직인다.*/
+	else if (m_movingDirect == false && m_isOn == true)
+	{
+		if (curDistance >= m_distance - 0.01f)
+		{
+			m_vPos.x = m_onPosX + m_distance;
+			m_isOn = false;
+		}
+		else
+			m_vPos.x += (m_vPos.x - m_onPosX) * movingRate + movingRate;
+	}
+
+
+
+	D3DXMATRIX mScaling, mTranslation;
+	D3DXMatrixScaling(&mScaling, m_width, m_height, 1.0f);
+	D3DXMatrixTranslation(&mTranslation, m_vPos.x, m_vPos.y, m_vPos.z);
+
+	m_localXform = mScaling * mTranslation;
+}
---
+++ Src/EmptyProject/SkillDescBox.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Picture.h"
+
+class SkillDescBox : public Picture
+{
+public:
+	void setOnPos (float x, float y, float z);
+	void setDistance (float distance) { m_distance = distance; }
+	void onBox();
+	void offBox();
+	bool isOn();
+
+	void setOff () { m_isOn = false; }
+
+	void frameMove( float fElapsedTime );
+
+private:
+	float m_onPosX;
+	float m_distance;
+	bool m_movingDirect;
+	bool m_isOn;
+};

96acef779a4633064a39615011a6b281abda8f13
Geoyeob Kim <gasbank@gmail.com>
2008-08-20 13:00:32 +0000

    - CODECHANGE: Unit.h reformat :)
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@460 20279357-db7c-4b2f-af4c-8ae08146ad34

--- Src/EmptyProject/Unit.h
+++ Src/EmptyProject/Unit.h
@@ -30,133 +30,126 @@ enum UnitType { UT_UNIT, UT_CHARACTER, UT_HERO, UT_ENEMY, UT_ATTACKOBJECT, UT_IN
 class Unit
 {
 public:
+	static Unit*					createUnit( LPD3DXMESH mesh, int tileX = 0, int tileY = 0, float posZ = 0 );
+	virtual							~Unit();
+
+	HRESULT							init( LPDIRECT3DDEVICE9 pd3dDevice, LPD3DXMESH mesh );
+	const D3DXVECTOR3&				getLowerLeft() const	{ return m_lowerLeft; }
+	const D3DXVECTOR3&				getUpperRight() const	{ return m_upperRight; }
+	LPD3DXMESH						getMesh() const			{ return m_d3dxMesh; }
+	void							release()				{ SAFE_RELEASE(m_d3dxMesh); }
+	void							clearKey();
+
+	virtual HRESULT					frameRender();
+	virtual LRESULT					handleMessages( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
+	virtual bool					frameMove( float fElapsedTime );
+
+	void							setRotX( float rad ) { m_vRot.x = rad; m_bLocalXformDirty = true; }
+	void							setRotY( float rad ) { m_vRot.y = rad; m_bLocalXformDirty = true; }
+	void							setRotZ( float rad ) { m_vRot.z = rad; m_bLocalXformDirty = true; }
+
+	void							setUniformScale( float val ) { m_vScale.x = m_vScale.y = m_vScale.z = val; m_bLocalXformDirty = true; }
+	void							setScaleX( float val )		{ m_vScale.x = val; m_bLocalXformDirty = true; }
+	void							setScaleY( float val )		{ m_vScale.y = val; m_bLocalXformDirty = true; }
+	void							setScaleZ( float val )		{ m_vScale.z = val; m_bLocalXformDirty = true; }
+
+	void							setPos( float x, float y, float z )	{ m_vPos.x = x; m_vPos.y = y; m_vPos.z = z; m_bLocalXformDirty = true; }
+	void							setPos( const D3DXVECTOR3& val )	{ m_vPos = val; m_bLocalXformDirty = true; }
+	void							setPosX( float val )				{ m_vPos.x = val; m_bLocalXformDirty = true; }
+	void							setPosY( float val )				{ m_vPos.y = val; m_bLocalXformDirty = true; }
+	void							setPosZ( float val )				{ m_vPos.z = val; m_bLocalXformDirty = true; }
+	virtual const D3DXVECTOR3&		getPos() const						{ return m_vPos; }
+
+	void							setHeadDir( UnitInput ui );
+
+	void							setTilePos( int tileX, int tileY );
+	void							setTilePos( const Point2Uint& newPos );
+	void							setTileBufferPos( int tileX, int tileY ) { m_tileBufferPos.x = tileX; m_tileBufferPos.y = tileY; }
 	
-
-	static Unit* createUnit( LPD3DXMESH mesh, int tileX = 0, int tileY = 0, float posZ = 0 );
-	virtual ~Unit();
-
-	HRESULT init(LPDIRECT3DDEVICE9 pd3dDevice, LPD3DXMESH mesh);
-	const D3DXVECTOR3& getLowerLeft() const { return m_lowerLeft; }
-	const D3DXVECTOR3& getUpperRight() const { return m_upperRight; }
-	LPD3DXMESH getMesh() const { return m_d3dxMesh; }
-	void release() { SAFE_RELEASE(m_d3dxMesh); }
-	void clearKey();
-
-	virtual HRESULT frameRender();
-	virtual LRESULT handleMessages( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-	virtual bool frameMove(float fElapsedTime);
-
-	void setRotX(float rad) { m_vRot.x = rad; m_bLocalXformDirty = true; }
-	void setRotY(float rad) { m_vRot.y = rad; m_bLocalXformDirty = true; }
-	void setRotZ(float rad) { m_vRot.z = rad; m_bLocalXformDirty = true; }
-
-	void setUniformScale(float val) { m_vScale.x = m_vScale.y = m_vScale.z = val; m_bLocalXformDirty = true; }
-	void setScaleX(float val) { m_vScale.x = val; m_bLocalXformDirty = true; }
-	void setScaleY(float val) { m_vScale.y = val; m_bLocalXformDirty = true; }
-	void setScaleZ(float val) { m_vScale.z = val; m_bLocalXformDirty = true; }
-
-	void setPos(float x, float y, float z) { m_vPos.x = x; m_vPos.y = y; m_vPos.z = z; m_bLocalXformDirty = true; }
-	void setPos(const D3DXVECTOR3& val) { m_vPos = val; m_bLocalXformDirty = true; }
-	void setPosX(float val) { m_vPos.x = val; m_bLocalXformDirty = true; }
-	void setPosY(float val) { m_vPos.y = val; m_bLocalXformDirty = true; }
-	void setPosZ(float val) { m_vPos.z = val; m_bLocalXformDirty = true; }
-	virtual const D3DXVECTOR3& getPos() const { return m_vPos; }
-
-	void setHeadDir( UnitInput );
-
-	void setTilePos(int tileX, int tileY);
-	void setTilePos( const Point2Uint& newPos );
-	void setTileBufferPos(int tileX, int tileY) { m_tileBufferPos.x = tileX; m_tileBufferPos.y = tileY; }
+	UINT							getTilePosX() const						{ return m_tilePos.x; }
+	UINT							getTilePosY() const						{ return m_tilePos.y; }
+	void							getTilePos( UINT& x, UINT& y ) const	{ x = m_tilePos.x; y = m_tilePos.y; }
 	
-	UINT getTilePosX() const { return m_tilePos.x; }
-	UINT getTilePosY() const { return m_tilePos.y; }
-	void getTilePos( UINT& x, UINT& y ) const { x = m_tilePos.x; y = m_tilePos.y; }
+	const D3DXMATRIX&				getLocalXform() const					{ return m_localXform; }
+
+	const Point2Uint&				getTilePos() const						{ return m_tilePos; }
+	const Point2Uint&				getTileBufferPos() const				{ return m_tileBufferPos; }
 	
-	const D3DXMATRIX& getLocalXform() const { return m_localXform; }
+	void							setRemoveFlag( bool flag )				{ m_removeFlag = flag; }
+	bool							getRemoveFlag()							{ return m_removeFlag; }
 
-	const Point2Uint& getTilePos() const { return m_tilePos; }
-	const Point2Uint& getTileBufferPos() const { return m_tileBufferPos; }
+	bool							getMovable() const						{ return m_bMovable; }
+	void							setMovable( bool val )					{ m_bMovable = val; }
+
+	UnitType						getType() const							{ return m_type; }
+	World*							getCurWorld()							{ return GetWorldManager().getCurWorld(); }
+	void							setDynamicMotion( DynamicMotion* dm )	{ m_dm = dm; }
+	void							setForcedMove( int i );
+	void							forcedMoveTest();
+	void							setArnMesh( ArnMesh* arnMesh )			{ assert( arnMesh ); m_arnMesh = arnMesh; }
 	
-	UnitType getType() const { return m_type; }
-	void setRemoveFlag( bool flag ) { m_removeFlag = flag; }
-	bool getRemoveFlag() { return m_removeFlag; }
-
-	World* getCurWorld() { return GetWorldManager().getCurWorld(); }
-	void setDynamicMotion (DynamicMotion* dm) { m_dm = dm; }
-
-	void setForcedMove( int i );
-	void forcedMoveTest();
+	void							setColor( int r, int g, int b );
 	
-	void setArnMesh( ArnMesh* arnMesh ) { assert( arnMesh ); m_arnMesh = arnMesh; }
-
-	HRESULT rayTesting( UnitInput );
-	bool getMovable() const { return m_bMovable; }
-	void setMovable(bool val) { m_bMovable = val; }
-
-	void setColor( int r, int g, int b );
-
-	void setLocalXformDirty() { m_bLocalXformDirty = true; }
-	bool isForcedMove() const { return m_bForcedMove; }
-
-	void startSoulAnimation( float duration, float height );
-
-	void setViewAt( const D3DXVECTOR3* at );
+	bool							isForcedMove() const					{ return m_bForcedMove; }
+	void							startSoulAnimation( float duration, float height );
+	void							setViewAt( const D3DXVECTOR3* at );
 
 protected:
-	Unit( UnitType type );
+									Unit( UnitType type );
+	void							setLocalXformDirty()					{ m_bLocalXformDirty = true; }
+	HRESULT							rayTesting( UnitInput ui );
 
-	BYTE					m_aKeys[UNIT_MAX_KEYS];
-	UINT					m_cKeysDown;            // Number of camera keys that are down.
-	D3DXVECTOR3				m_vKeyboardDirection;
-	D3DXVECTOR3				m_vVelocity;
-	LPD3DXMESH				m_d3dxMesh;
-	LPDIRECT3DTEXTURE9		m_d3dTex;
+	BYTE							m_aKeys[UNIT_MAX_KEYS];
+	UINT							m_cKeysDown;            // Number of camera keys that are down.
+	D3DXVECTOR3						m_vKeyboardDirection;
+	D3DXVECTOR3						m_vVelocity;
+	LPD3DXMESH						m_d3dxMesh;
+	LPDIRECT3DTEXTURE9				m_d3dTex;
 
-	virtual UnitInput mapKey( UINT nKey );
+	virtual UnitInput				mapKey( UINT nKey );
 
 private:
-	World* getWorldState() const;
-
-	void drawSoul();
-	void updateSoulAnimation( float fElapsedTime );
+	World*							getWorldState() const;
+	void							drawSoul();
+	void							updateSoulAnimation( float fElapsedTime );
 
 	struct TeapotVertex
 	{
 		float x, y, z;
 		float nx, ny, nz;
 	};
-	void updateLocalXform();
+	void							updateLocalXform();
 	
-	D3DXVECTOR3				m_lowerLeft, m_upperRight; // bounding box params
-	D3DXVECTOR3				m_vRot, m_vPos, m_vScale;
-	bool					m_bLocalXformDirty;
-	D3DXMATRIX				m_localXform;
-	LPDIRECT3DDEVICE9		m_pd3dDevice;
-	D3DMATERIAL9			m_material;
+	D3DXVECTOR3						m_lowerLeft, m_upperRight; // bounding box params
+	D3DXVECTOR3						m_vRot, m_vPos, m_vScale;
+	bool							m_bLocalXformDirty;
+	D3DXMATRIX						m_localXform;
+	LPDIRECT3DDEVICE9				m_pd3dDevice;
+	D3DMATERIAL9					m_material;
 	
-	bool					m_removeFlag;
-	bool					m_bForcedMove;
+	bool							m_removeFlag;
+	bool							m_bForcedMove;
 	
-	DynamicMotion*			m_dm;
+	DynamicMotion*					m_dm;
 	
 	
 
-	Point2Uint				m_tilePos;
-	Point2Uint				m_tileBufferPos;
+	Point2Uint						m_tilePos;
+	Point2Uint						m_tileBufferPos;
 
-	UnitType				m_type;
+	UnitType						m_type;
 
-	ArnMesh*				m_arnMesh;
-	bool					m_bMovable;
+	ArnMesh*						m_arnMesh;
+	bool							m_bMovable;
 
-	float					m_fSoulAnimationTimer;
-	float					m_fSoulAnimationDuration;
-	float					m_fSoulAnimationHeight;
-	D3DXMATRIX				m_localXformSoul;
-	D3DMATERIAL9			m_materialSoul;
-	bool					m_bSoulAnimation;
-	D3DXMATRIX				m_prevLocalXform;
-	D3DMATERIAL9			m_prevMaterial;
+	float							m_fSoulAnimationTimer;
+	float							m_fSoulAnimationDuration;
+	float							m_fSoulAnimationHeight;
+	D3DXMATRIX						m_localXformSoul;
+	D3DMATERIAL9					m_materialSoul;
+	bool							m_bSoulAnimation;
+	D3DXMATRIX						m_prevLocalXform;
+	D3DMATERIAL9					m_prevMaterial;
 };
 
 SCRIPT_FACTORY( Unit )

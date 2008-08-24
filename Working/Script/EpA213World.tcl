817c9fdf8f908d2c8e67e12c7cfa96ee9a9934e2
Youngshin Kim <luneride@hotmail.com>
2008-08-24 12:06:57 +0000

    - NEW: 퀘스트 미완성 일단 커밋.
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@557 20279357-db7c-4b2f-af4c-8ae08146ad34

--- Working/Script/EpA213World.tcl
+++ Working/Script/EpA213World.tcl
@@ -1,8 +1,8 @@
 namespace eval EpA213World {
 	set modelFilePath	"A213World.arn"
-	set dialogNameList [ list laserDialog openDialog NPC_OldRatDialog NPC_OldCatDialog ]
+	set dialogNameList [ list laserDialog openDialog NPC_OldRatDialog NPC_OldCat1Dialog NPC_OldCat10Dialog ]
 	variable pHeroUnit
-	global pNPC_OldRat pEnemy_OldCat
+	global pNPC_OldRat pEnemy_OldCat1 pEnemy_OldCat10
 	variable gatePos { 14 66 }
 	variable ratholPos { 12 50 }
 	
@@ -20,7 +20,7 @@ namespace eval EpA213World {
 		variable ratholPos
 		variable Box
 		variable Mirror
-		global pNPC_OldRat pEnemy_OldCat
+		global pNPC_OldRat pEnemy_OldCat1 pEnemy_OldCat10
 		
 		EpOutputDebugString " - [info level 0] called\n"
 		
@@ -123,14 +123,23 @@ namespace eval EpA213World {
 		EpUnitSetName				$pNPC_OldRat "An Old Rat..."
 		EpUnitSetNameVisible		$pNPC_OldRat 1
 
-		set	pEnemy_OldCat			[ createEnemy 21 70 ]
-		EpUnitSetArnMesh			$pEnemy_OldCat "GwengYiModel"
-		EpUnitSetColor				$pEnemy_OldCat 128 128 128
-		EpCharacterSetStat			$pEnemy_OldCat 10 10 10 10 10 10
-		EpCharacterSetCurHp			$pEnemy_OldCat 100
-		EpEnemySetTalkable			$pEnemy_OldCat 1
-		EpUnitSetName				$pEnemy_OldCat "Very Strong Gatekeeper"
-		EpUnitSetNameVisible		$pEnemy_OldCat 1
+		set	pEnemy_OldCat1			[ createEnemy 21 65 ]
+		EpUnitSetArnMesh			$pEnemy_OldCat1 "GwengYiModel"
+		EpUnitSetColor				$pEnemy_OldCat1 128 128 128
+		EpCharacterSetStat			$pEnemy_OldCat1 10 10 10 10 10 10
+		EpCharacterSetCurHp			$pEnemy_OldCat1 100
+		EpEnemySetTalkable			$pEnemy_OldCat1 1
+		EpUnitSetName				$pEnemy_OldCat1 "No.1 Gatekeeper"
+		EpUnitSetNameVisible		$pEnemy_OldCat1 1
+
+		set	pEnemy_OldCat10			[ createEnemy 21 70 ]
+		EpUnitSetArnMesh			$pEnemy_OldCat10 "GwengYiModel"
+		EpUnitSetColor				$pEnemy_OldCat10 128 128 128
+		EpCharacterSetStat			$pEnemy_OldCat10 10 10 10 10 10 10
+		EpCharacterSetCurHp			$pEnemy_OldCat10 100
+		EpEnemySetTalkable			$pEnemy_OldCat10 1
+		EpUnitSetName				$pEnemy_OldCat10 "No.10 Gatekeeper"
+		EpUnitSetNameVisible		$pEnemy_OldCat10 1
 
 		set Box					[createStructureObject 6 70];
 		EpUnitSetArnMesh			$Box "PushableBox"
@@ -146,7 +155,8 @@ namespace eval EpA213World {
 		registerIncidentOpen
 		registerIncidentOpen2
 		EpRegisterInitFadeInIncident
-		Incident_TalkWithOldRat
+		registerIncident_TalkWithOldRat
+		registerIncident_pEnemy_OldCat1
 	}
 	
 	
@@ -341,18 +351,18 @@ namespace eval EpA213World {
 
 	# 설명 : 늙은 쥐와의 대화를 통해 A213에 얽힌 전설을 듣게 되고, 문지기를 해치우고 A213의 문을
 	# 열기 위한 필수 아이템 상한 두부를 얻는다.
-	proc Incident_TalkWithOldRat {} {
+	proc registerIncident_TalkWithOldRat {} {
 		variable pHeroUnit
-		global pNPC_OldRat pEnemy_OldCat
+		global pNPC_OldRat pEnemy_OldCat10
 		set animObjects [ list Blocking1 Blocking2 Blocking3 GateRight GateLeft GateCamera ]
 
-		set sqIncident		[ EpCreateSequentialIncident -1 ]
+		set sqIncident		[ EpCreateSequentialIncident 1 ]
 
 		set trigger0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pNPC_OldRat 0x001 ]
 		set action0			[ EpCreateDialogAction "EpA213World::NPC_OldRatDialog" ]
 
-		set trigger1		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_OldCat 0x001 ]
-		set action1			[ EpCreateDialogAction "EpA213World::NPC_OldCatDialog" ]
+		set trigger1		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_OldCat10 0x001 ]
+		set action1			[ EpCreateDialogAction "EpA213World::NPC_OldCat10Dialog" ]
 		#set action2			[ EpCreateScriptAction "Incident_GateOpen" ]
 
 		EpAddTriggerToSequence		$sqIncident $trigger0
@@ -383,6 +393,26 @@ namespace eval EpA213World {
 		
 		set incCount		[ EpRegisterSequentialIncident $sqIncident ]
 		EpOutputDebugString " - Incident count: $incCount\n"
+	}
+
+	proc registerIncident_pEnemy_OldCat1 {} {
+		variable pHeroUnit
+		global pNPC_OldRat pEnemy_OldCat1
+
+		set trigger		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_OldCat1 0x001 ]
+		set actions		[ EpCreateDialogAction "EpA213World::NPC_OldCat1Dialog" ]
+		lappend actions		[ EpCreateScriptAction "EpEnemySetTalkable $pEnemy_OldCat1 0" ]
+				
+		set incident	[ EpCreateBlockingActionIncident $trigger 0 1 ]
+
+		foreach act $actions {
+			EpAddActionToIncident $incident $act
+		}
+
+		EpIncidentSetName	$incident "pEnemy_OldCat1 incident"
+		
+		EpOutputDebugString " - pEnemy_OldCat1 Incident returned $incident\n"
+		return $incident
 	}
 	
 	proc leave {} {
@@ -419,6 +449,7 @@ namespace eval EpA213World {
 	
 		set player "PoolG"
 		set npc "An Old Rat..."
+		set	system "System"
 	
 		set dialog [ list\
 			$npc		"자네도 Dark Lord Choi를 만나러 왔는가?"\
@@ -436,7 +467,7 @@ namespace eval EpA213World {
 			$npc		"그리고 그 이후로 나는 계속 여기에서 살고 있지..."\
 			$player		"왜 여기서 사십니까?"\
 			$npc		"자네같은 친구를 위해서일세"\
-			$npc		"내가 나온 그날 이후로, 이 문을 넘어가서 살아 돌아온 쥐는 없었다네."\
+			$npc		"내가 나온 그날 이후로, 저 문을 넘어가서 살아 돌아온 쥐는 없었다네."\
 			$npc		"자네도 나처럼 모든 것을 잃지 말고, 돌아가게나."\
 			$player		"저는 이미 대청소로 모든 것을 잃었습니다."\
 			$player		"반드시 저 문을 지나서, 그에게 복수해야만 합니다."\
@@ -444,15 +475,16 @@ namespace eval EpA213World {
 			$npc		"굳이 가겠다면, 내 부탁 하나만 들어주겠나?"\
 			$player		"무엇입니까?"\
 			$npc		"내가 쓰레기통에서 먹다 남은 두부 캔을 하나 주웠네"\
-			$npc		"저기 북동쪽의 문지기와 이것을 나누어 먹고 싶은데, 나는 늙고 병들어서 움직이기가 귀찮아."\
+			$npc		"저기 북동쪽의 2번 문지기와 이것을 나누어 먹고 싶은데, 나는 늙고 병들어서 움직이기가 귀찮아."\
 			$npc		"자네가 이것을 좀 가져다 주게."\
 			$player		"네"\
+			$system		"상한 두부를(을) 얻었습니다."\
 		];	
 	}
 
-	namespace eval NPC_OldCatDialog {} {
+	namespace eval NPC_OldCat10Dialog {} {
 	
-		set region [ list 0 0 0 0 ]; ;# left, top, right, bottom
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
 		set oneTime 0;
 	
 		set player "PoolG"
@@ -472,6 +504,22 @@ namespace eval EpA213World {
 			$npc		"상했잖아!!!!"\
 			$npc		"(털썩)"\
 			$system		"당신은 쓰러진 문지기로부터 열쇠를 얻어서 구멍에 끼워 돌려 보았다."\
+		];	
+	}
+
+	namespace eval NPC_OldCat1Dialog {} {
+		
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+	
+		set player "PoolG"
+		set npc "Old Cat(Gatekeeper)"
+		set	system "System"
+	
+		set dialog [ list\
+			$npc		"이게 무슨 냄새지?"\
+			$player		"두부"\
+			$npc		"괘씸하다! 난 두부가 싫타!"\
 		];	
 	}
 }

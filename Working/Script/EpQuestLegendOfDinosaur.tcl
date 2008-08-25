d395ddee98eba98707e1e7cf30a1406cbe289f40
Youngshin Kim <luneride@hotmail.com>
2008-08-25 05:33:35 +0000

    - FIX: Some missing script files.
    git-svn-id: svn://gasbank.kaist.ac.kr/poolg/trunk@577 20279357-db7c-4b2f-af4c-8ae08146ad34

---
+++ Working/Script/EpQuestLegendOfDinosaur.tcl
@@ -0,0 +1,127 @@
+namespace eval EpA213World {
+	# -----------------------------------------------------------------------------------------
+	# 제목 : 공룡의 전설
+	# 설명 : A213의 공룡들은 사실 원래 쥐였다. 이 쥐들은 A213에 들어왔다가 최재영을 만나서
+	# 본디의 코딩 스피릿의 어두운 면을 모두 잃고서 A213을 배회하는 괴물들이 되고 말았다.
+	# 하지만 아직 코딩 스피릿의 어두운 면을 다 잃지 않은 한 공룡(43, 54)이 주인공에게 "Big Rigs Limited
+	# Special Package"을 찾아오면 자신이 다시 쥐로 돌아수 있다고 주인공에게 부탁을 한다.
+	# 그리고 이 아이템은 A213 남서쪽 공간의 한 쥐(25, 45)가 가지고 있다.
+	# 주인공은 "Big Rigs..."를 가져오면서 다른 쥐들의 습격(12, 56)도 받는다.
+	proc registerIncident_LegendOfDinosaur {} {
+		variable pHeroUnit
+		global	pNPC_GoodDinosaur pEnemy_EvilRat
+
+		set seqIncident		[ EpCreateSequentialIncident 1 ]
+
+		set triggerA0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pNPC_GoodDinosaur 0x001 ]
+		set actionA0		[ EpCreateDialogAction "EpA213World::Dialog_LegendOfDinosaur_GoodDinosaur0" ]
+
+		set triggerB0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_EvilRat 0x001 ]
+		set actionB0		[ EpCreateDialogAction "EpA213World::Dialog_LegendOfDinosaur_EvilRat" ]
+		set actionB1		[ EpCreateScriptAction "EpEnemySetTalkable $pEnemy_EvilRat 0" ]
+
+		set triggerC0		[ EpCreateUnitPositionTrigger $pHeroUnit 10 54 14 53 0x001 ]
+		set actionC1		[ EpCreateScriptAction "createEnemy 11 55" ]
+		set actionC2		[ EpCreateScriptAction "createEnemy 13 55" ]
+		set actionC3		[ EpCreateDialogAction "EpA213World::Dialog_LegendOfDinosaur_BigRigsAssault" ]
+
+		set triggerD0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pNPC_GoodDinosaur 0x001 ]
+		set actionD0		[ EpCreateDialogAction "EpA213World::Dialog_LegendOfDinosaur_GoodDinosaur1" ]
+		# TODO: 착한 공룡이 사라지거나 쥐로 변하거나 해야 한다.
+
+		EpAddTriggerToSequence	$seqIncident $triggerA0
+		EpAddActionToSequence	$seqIncident $actionA0
+
+		EpAddTriggerToSequence	$seqIncident $triggerB0
+		EpAddActionToSequence	$seqIncident $actionB0
+		EpAddActionToSequence	$seqIncident $actionB1
+
+		EpAddTriggerToSequence	$seqIncident $triggerC0
+		EpAddActionToSequence	$seqIncident $actionC1
+		EpAddActionToSequence	$seqIncident $actionC2
+		EpAddActionToSequence	$seqIncident $actionC3
+
+		EpAddTriggerToSequence	$seqIncident $triggerD0
+		EpAddActionToSequence	$seqIncident $actionD0
+
+		EpSequentialIncidentSetName	$seqIncident "LegendOfDinsaur sequential incident"
+
+		set incCount		[ EpRegisterSequentialIncident $seqIncident ]
+		
+		EpOutputDebugString " - LegendOfDinsaur Incident returned $seqIncident\n"
+		return $seqIncident
+	}
+
+	namespace eval Dialog_LegendOfDinosaur_GoodDinosaur0 {} {
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+
+		set player "PoolG"
+		set npc "Gentle Dinosaur"
+		set system "System"
+
+		set dialog [ list\
+			$npc		"이봐요"\
+			$player		"공룡이 말을 하다니!"\
+			$npc		"도와주세요."\
+			$player		"무엇을 도와드릴까요?"\
+			$npc		"여기 모든 공룡들은 원래 코딩 스피릿의 어두운 면이 충만한 당신과 같은 쥐들이었죠"\
+			$npc		"하지만 A213에 들어와 Dark Lord Choi의 무자비한 디자인 패턴 공격으로 코딩 스피릿을 모두 잃고"\
+			$npc		"이렇게 괴물이 되어 이곳을 배회하게 된 것이에요..."\
+			$player		"그렇군."\
+			$npc		"저도 곧 이성을 잃고 배회하는 괴물이 될 거에요."\
+			$npc		"하지만 그게 있다면..."\
+			$player		"그거라면, Big Rigs Limited Special Package를 말하는 건가?"\
+			$npc		"바로 그거에요!"\
+			$npc		"A213을 나가서 남쪽으로 가면 넓은 공터가 있어요."\
+			$npc		"거기에 제가 Big Rigs Limited Special Package를 숨겨 놓았어요."\
+			$player		"알겠네."\
+		];
+	}
+
+	namespace eval Dialog_LegendOfDinosaur_EvilRat {} {
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+
+		set player "PoolG"
+		set npc "Evil Rat"
+		set system "System"
+
+		set dialog [ list\
+			$player		"그 Big Rigs Limited Special Package는 어디서 난건가?"\
+			$npc		"My pressure..."\
+			$player		"내 놓으시지"\
+			$npc		"안돼! 이건 내거야!!"\
+		];
+	}
+
+	namespace eval Dialog_LegendOfDinosaur_BigRigsAssault {} {
+	set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+
+		set player "PoolG"
+		set npc "Assassin Rat"
+		set system "System"
+
+		set dialog [ list\
+			$npc		"Big Rigs Limited Special Package를 가지고 있군."\
+			$player		"누구냐!"\
+			$npc		"죽어줘야겠다."\
+		];
+	}
+
+	namespace eval Dialog_LegendOfDinosaur_GoodDinosaur1 {} {
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+
+		set player "PoolG"
+		set npc "Gentle Dinosaur"
+		set system "System"
+
+		set dialog [ list\
+			$npc		"가져오셨군요!"\
+			$npc		"정말 고마워요. 저는 이제 다시 쥐로 돌아갈 수 있게 되었어요."\
+			$player		"별말씀을"\
+		];
+	}
+}
---
+++ Working/Script/EpQuestOldRatandGatekeeper.tcl
@@ -0,0 +1,167 @@
+namespace eval EpA213World {
+# -----------------------------------------------------------------------------------------
+	# 제목 : 늙은 쥐의 전설과 문지기
+	# 설명 : 늙은 쥐와의 대화를 통해 A213에 얽힌 전설을 듣게 되고, 문지기를 해치우고 A213의 문을
+	# 열기 위한 필수 아이템 상한 두부를 얻는다.
+	# registerIncident_TalkWithOldRat
+	# registerIncident_pEnemy_OldCat1 
+	proc registerIncident_TalkWithOldRat {} {
+		variable pHeroUnit
+		global pNPC_OldRat pEnemy_OldCat10 pEnemy_OldCat11
+		set animObjects [ list Blocking1 Blocking2 Blocking3 GateRight GateLeft GateCamera ]
+
+		set sqIncident		[ EpCreateSequentialIncident 1 ]
+
+		set trigger0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pNPC_OldRat 0x001 ]
+		set action0			[ EpCreateDialogAction "EpA213World::NPC_OldRatDialog" ]
+
+		set action1			[ EpCreateScriptAction "EpEnemySetTalkable pEnemy_OldCat1 1" ]
+
+		set trigger1		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_OldCat10 0x001 ]
+		set action1			[ EpCreateDialogAction "EpA213World::NPC_OldCat10Dialog" ]
+		#set action2			[ EpCreateScriptAction "Incident_GateOpen" ]
+
+		EpAddTriggerToSequence		$sqIncident $trigger0
+		EpAddActionToSequence		$sqIncident	$action0
+		EpAddTriggerToSequence		$sqIncident	$trigger1
+		EpAddActionToSequence		$sqIncident	$action1
+		#EpAddActionToSequence		$sqIncident	$action2
+
+		# 문 열기 ---------------------------------------------------------------------------------
+		set actions			[ EpCreateControllableAction	$pHeroUnit 0 ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode Blocking1  ] 1" ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode Blocking2  ] 1" ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode Blocking3  ] 1" ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode GateRight  ] 1" ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode GateLeft   ] 1" ]
+		lappend actions		[ EpCreateScriptAction			"EpSetDoAnim [ EpGetNode GateCamera ] 1" ]
+		lappend actions		[ EpCreateCameraAction			external GateCamera 0 ]
+		lappend actions		[ EpCreateDelayAction			10000 ]
+		lappend actions		[ EpCreateCameraAction			attach GateCamera 1500 ]
+		lappend actions		[ EpCreateControllableAction	$pHeroUnit 1 ]
+
+		foreach act $actions {
+			EpAddActionToSequence $sqIncident $act
+		}
+		# ------------------------------------------------------------------------------------------
+
+		set action					[ EpCreateScriptAction	"EpEnemySetTalkable $pEnemy_OldCat11 0" ]
+		EpAddActionToSequence		$sqIncident	$action
+		set action					[ EpCreateScriptAction	"EpUnitSetRemoveFlag $pEnemy_OldCat10 1" ]
+		EpAddActionToSequence		$sqIncident $action
+		
+		EpSequentialIncidentSetName	$sqIncident "TalkWithRat incident"
+		
+		set incCount		[ EpRegisterSequentialIncident $sqIncident ]
+		EpOutputDebugString " - Incident count: $incCount\n"
+	}
+
+	proc registerIncident_pEnemy_OldCat1 {} {
+		variable pHeroUnit
+		global pNPC_OldRat pEnemy_OldCat1 pEnemy_OldCat11
+
+		set trigger0		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pNPC_OldRat 0x001 ]
+		set trigger1		[ EpCreateUnitPositionWithTraceTrigger $pHeroUnit $pEnemy_OldCat1 0x001 ]
+		set action0		[ EpCreateScriptAction "#" ]
+		set action1		[ EpCreateDialogAction "EpA213World::NPC_OldCat1Dialog" ]
+		set action2		[ EpCreateScriptAction "EpEnemySetTalkable $pEnemy_OldCat1 0" ]
+				
+		set seqIncident	[ EpCreateSequentialIncident 1 ]
+		EpAddTriggerToSequence $seqIncident $trigger0
+		EpAddActionToSequence $seqIncident $action0
+		EpAddTriggerToSequence $seqIncident $trigger1
+		EpAddActionToSequence $seqIncident $action1
+		EpAddActionToSequence $seqIncident $action2
+
+		set action					[ EpCreateScriptAction	"EpEnemySetTalkable $pEnemy_OldCat11 0" ]
+		EpAddActionToSequence		$seqIncident	$action
+
+		EpSequentialIncidentSetName	$seqIncident "pEnemy_OldCat1 sequential incident"
+
+		set incCount		[ EpRegisterSequentialIncident $seqIncident ]
+		
+		EpOutputDebugString " - pEnemy_OldCat1 Incident returned $seqIncident\n"
+		return $seqIncident
+	}
+
+		namespace eval NPC_OldRatDialog {} {
+	
+		set region [ list 0 0 0 0 ]; ;# left, top, right, bottom
+		set oneTime 0;
+	
+		set player "PoolG"
+		set npc "An Old Rat..."
+		set	system "System"
+	
+		set dialog [ list\
+			$npc		"자네도 Dark Lord Choi를 만나러 왔는가?"\
+			$player		"....."\
+			$npc		"자네의 깊고 어두운 코딩 스피릿을 감지하고 알 수 있었지.."\
+			$npc		"왜 그에게 도전하려 하는겐가?"\
+			$player		"복수하기 위해서 입니다."\
+			$npc		"그렇군..."\
+			$npc		"나도 자네처럼 젊은 때가 있었지."\
+			$npc		"A213에는 전력과 냉방이 풍부하다는 소문이 있었어."\
+			$npc		"동료들을 모아 모험을 떠났고, 여기에 도착했지. 자네처럼 말이야..."\
+			$npc		"그리고 그를 만났지..."\
+			$npc		"그는 정말 강했어! Ctr-C, Ctr-V 공격에 그는 눈 하나 깜박하지 않았어!"\
+			$npc		"동료들은 모두 죽었고, 나는 비겁하게 혼자 살아 나왔다네."\
+			$npc		"그리고 그 이후로 나는 계속 여기에서 살고 있지..."\
+			$player		"왜 여기서 사십니까?"\
+			$npc		"자네같은 친구를 위해서일세"\
+			$npc		"내가 나온 그날 이후로, 저 문을 넘어가서 살아 돌아온 쥐는 없었다네."\
+			$npc		"자네도 나처럼 모든 것을 잃지 말고, 돌아가게나."\
+			$player		"저는 이미 대청소로 모든 것을 잃었습니다."\
+			$player		"반드시 저 문을 지나서, 그에게 복수해야만 합니다."\
+			$npc		"소용이 없구먼.."\
+			$npc		"굳이 가겠다면, 내 부탁 하나만 들어주겠나?"\
+			$player		"무엇입니까?"\
+			$npc		"내가 쓰레기통에서 먹다 남은 두부 캔을 하나 주웠네"\
+			$npc		"저기 북동쪽의 2번 문지기와 이것을 나누어 먹고 싶은데, 나는 늙고 병들어서 움직이기가 귀찮아."\
+			$npc		"자네가 이것을 좀 가져다 주게."\
+			$player		"네"\
+			$system		"상한 두부를(을) 얻었습니다."\
+		];	
+	}
+
+	namespace eval NPC_OldCat10Dialog {} {
+	
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+	
+		set player "PoolG"
+		set npc "Old Cat(Gatekeeper)"
+		set	system "System"
+	
+		set dialog [ list\
+			$npc		"두부 냄새가 나는군."\
+			$player		"조금 드릴까요?"\
+			$npc		"응 그러면 고맙겠어."\
+			$player		"여기요."\
+			$npc		"(냠냠...)"\
+			$npc		"(쩝쩝...)"\
+			$npc		"(우걱우걱...)"\
+			$npc		"(꿀꺽!)"\
+			$npc		"읔!!!!!!!!!!!"\
+			$npc		"상했잖아!!!!"\
+			$npc		"(털썩)"\
+			$system		"당신은 쓰러진 문지기로부터 열쇠를 얻어서 구멍에 끼워 돌려 보았다."\
+		];	
+	}
+
+	namespace eval NPC_OldCat1Dialog {} {
+		
+		set region [ list 0 0 0 0 ] ;# left, top, right, bottom
+		set oneTime 0;
+	
+		set player "PoolG"
+		set npc "Old Cat(Gatekeeper)"
+		set	system "System"
+	
+		set dialog [ list\
+			$npc		"이게 무슨 냄새지?"\
+			$player		"두부"\
+			$npc		"괘씸하다! 내가 제일 싫어하는 두부를 가져오다니!"\
+		];	
+	}
+}
